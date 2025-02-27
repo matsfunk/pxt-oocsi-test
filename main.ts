5005627289a24a910b3d616ed11e7aecd8d0da8f
matsfunk <mathias.funk@gmail.com>
2025-02-27 22:23:31 +0100

    code organization

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 /*******************************************************************************
- * MakeCode extension for OOCSI via ESP8266 Wifi module.
+ * Functions for OOCSI
  *
- * Company: Cytron Technologies Sdn Bhd
- * Website: http://www.cytron.io
- * Email:   support@cytron.io
+ * Company: Mathias Funk, Industrial Design, TU/e
+ * Website: https://oocsi.net
+ * Email:   m.funk@tue.nl
  *******************************************************************************/
 
 /**
@@ -11,251 +11,116 @@
  */
 //% weight=10 color=#9236A4 icon="\uf1eb" block="OOCSI ESP8266"
 namespace oocsi {
-    // Flag to indicate whether the ESP8266 was initialized successfully.
-    let esp8266Initialized = false
 
-    // Buffer for data received from UART.
-    let rxData = ""
+    let lastMessage : { [key: string] : { value: any } } = {};
 
     /**
-     * Send AT command and wait for response.
-     * Return true if expected response is received.
-     * @param command The AT command without the CRLF.
-     * @param expected_response Wait for this response.
-     * @param timeout Timeout in milliseconds.
+     * Connect to OOCSI
+     * @param server OOCSI server. eg: "super.oocsi.net"
+     * @param name Client name. eg: "MicroBit_client_####"
      */
-    //% blockHidden=true
-    //% blockId=esp8266_send_command
-    export function sendCommand(command: string, expected_response: string = null, timeout: number = 100): boolean {
-        // Wait a while from previous command.
-        basic.pause(10)
+    //% weight=20
+    //% blockGap=8
+    //% blockId=oocsi_connect
+    //% block="connect to OOCSI: Server %channel Name %name"
+    export function connect(server: string, name: string) {
 
-        // Flush the Rx buffer.
-        serial.readString()
-        rxData = ""
+        sendCommand("AT+CIPSTART=\"TCP\",\"" + server + "\",4444", "OK", 10000)
 
-        // Send the command and end with "\r\n".
-        serial.writeString(command + "\r\n")
-        
-        // Don't check if expected response is not specified.
-        if (expected_response == null) {
+        pause(500)
+
+        let data = name + "(JSON)\r\n"
+        sendCommand("AT+CIPSEND=" + (data.length + 2))
+        sendCommand(data)
+
+    }
+
+    /**
+     * Send to OOCSI
+     * @param channel OOCSI channel.
+     * @param key Key to send.
+     * @param value Value to send.
+     */
+    //% weight=19
+    //% blockGap=8
+    //% blockId=oocsi_send
+    //% block="send to OOCSI: Channel %channel Key %key Value %value"
+    export function send(channel: string, key: string, value: string) {
+
+        // Make sure the WiFi is connected.
+        if (isWifiConnected() == false) return
+
+        // prepare and send data
+        let data = `sendjson ${channel} {"${key}": "${value}"}` + "\r\n"
+        sendCommand("AT+CIPSEND=" + (data.length + 2))
+        sendCommand(data)
+
+    }
+
+    /**
+     * Subscribe to OOCSI channel
+     * @param channel OOCSI channel.
+     */
+    //% weight=18
+    //% blockGap=8
+    //% blockId=oocsi_subscribe
+    //% block="subscribe to OOCSI: Channel %channel
+    export function subscribe(channel: string) {
+
+        // Make sure the WiFi is connected.
+        if (isWifiConnected() == false) return
+
+        // prepare and send data
+        let data = `subscribe ${channel}` + "\r\n"
+        sendCommand("AT+CIPSEND=" + (data.length + 2))
+        sendCommand(data)
+
+    }
+
+    /**
+     * Check for new incoming OOCSI messages
+     */
+    //% weight=17
+    //% blockGap=8
+    //% blockId=oocsi_check
+    //% block="check OOCSI"
+    export function check() : boolean {
+
+        // Make sure the WiFi is connected.
+        if (isWifiConnected() == false) return false
+
+        let line: string = getResponse("+IPD", 500)
+
+        // nothing received?
+        if(line == undefined || line.trim().length == 0 || line.indexOf('{') == -1) {
+            return false;
+        }
+
+        // try parse line
+        try {
+            const jsonString = line.substr(line.indexOf('{'));
+            lastMessage = JSON.parse(jsonString)
             return true
+        } catch(err) {
+            lastMessage = {}
+            return false            
         }
+
+    }
+
+    /**
+     * Get a value from the last OOCSI message
+     * @param key Key of the value.
+     * @param defaultValue Default value in case there is no value for the key.
+     */
+    //% weight=16
+    //% blockGap=8
+    //% blockId=oocsi_get
+    //% block="get value from last OOCSI message"
+    export function get(key: string, defaultValue: string) : string {
+
+        return convertToText(lastMessage != undefined && lastMessage[key] ? lastMessage[key] : defaultValue);
         
-        // Wait and verify the response.
-        let result = false
-        let timestamp = input.runningTime()
-        while (true) {
-            // Timeout.
-            if (input.runningTime() - timestamp > timeout) {
-                result = false
-                break
-            }
-
-            // Read until the end of the line.
-            rxData += serial.readString()
-            if (rxData.includes("\r\n")) {
-                // Check if expected response received.
-                if (rxData.slice(0, rxData.indexOf("\r\n")).includes(expected_response)) {
-                    result = true
-                    break
-                }
-
-                // If we expected "OK" but "ERROR" is received, do not wait for timeout.
-                if (expected_response == "OK") {
-                    if (rxData.slice(0, rxData.indexOf("\r\n")).includes("ERROR")) {
-                        result = false
-                        break
-                    }
-                }
-
-                // Trim the Rx data before loop again.
-                rxData = rxData.slice(rxData.indexOf("\r\n") + 2)
-            }
-        }
-
-        return result
     }
 
-
-
-    /**
-     * Get the specific response from ESP8266.
-     * Return the line start with the specific response.
-     * @param command The specific response we want to get.
-     * @param timeout Timeout in milliseconds.
-     */
-    //% blockHidden=true
-    //% blockId=esp8266_get_response
-    export function getResponse(response: string, timeout: number = 100): string {
-        let responseLine = ""
-        let timestamp = input.runningTime()
-        while (true) {
-            // Timeout.
-            if (input.runningTime() - timestamp > timeout) {
-                // Check if expected response received in case no CRLF received.
-                if (rxData.includes(response)) {
-                    responseLine = rxData
-                }
-                break
-            }
-
-            // Read until the end of the line.
-            rxData += serial.readString()
-            if (rxData.includes("\r\n")) {
-                // Check if expected response received.
-                if (rxData.slice(0, rxData.indexOf("\r\n")).includes(response)) {
-                    responseLine = rxData.slice(0, rxData.indexOf("\r\n"))
-
-                    // Trim the Rx data for next call.
-                    rxData = rxData.slice(rxData.indexOf("\r\n") + 2)
-                    break
-                }
-
-                // Trim the Rx data before loop again.
-                rxData = rxData.slice(rxData.indexOf("\r\n") + 2)
-            }
-        }
-
-        return responseLine
-    }
-
-
-
-    /**
-     * Format the encoding of special characters in the url.
-     * @param url The url that we want to format.
-     */
-    //% blockHidden=true
-    //% blockId=esp8266_format_url
-    export function formatUrl(url: string): string {
-        url = url.replaceAll("%", "%25")
-        url = url.replaceAll(" ", "%20")
-        url = url.replaceAll("!", "%21")
-        url = url.replaceAll("\"", "%22")
-        url = url.replaceAll("#", "%23")
-        url = url.replaceAll("$", "%24")
-        url = url.replaceAll("&", "%26")
-        url = url.replaceAll("'", "%27")
-        url = url.replaceAll("(", "%28")
-        url = url.replaceAll(")", "%29")
-        url = url.replaceAll("*", "%2A")
-        url = url.replaceAll("+", "%2B")
-        url = url.replaceAll(",", "%2C")
-        url = url.replaceAll("-", "%2D")
-        url = url.replaceAll(".", "%2E")
-        url = url.replaceAll("/", "%2F")
-        url = url.replaceAll(":", "%3A")
-        url = url.replaceAll(";", "%3B")
-        url = url.replaceAll("<", "%3C")
-        url = url.replaceAll("=", "%3D")
-        url = url.replaceAll(">", "%3E")
-        url = url.replaceAll("?", "%3F")
-        url = url.replaceAll("@", "%40")
-        url = url.replaceAll("[", "%5B")
-        url = url.replaceAll("\\", "%5C")
-        url = url.replaceAll("]", "%5D")
-        url = url.replaceAll("^", "%5E")
-        url = url.replaceAll("_", "%5F")
-        url = url.replaceAll("`", "%60")
-        url = url.replaceAll("{", "%7B")
-        url = url.replaceAll("|", "%7C")
-        url = url.replaceAll("}", "%7D")
-        url = url.replaceAll("~", "%7E")
-        return url
-    }
-
-
-
-    /**
-     * Return true if the ESP8266 is already initialized.
-     */
-    //% weight=30
-    //% blockGap=8
-    //% blockId=esp8266_is_esp8266_initialized
-    //% block="ESP8266 initialized"
-    export function isESP8266Initialized(): boolean {
-        return esp8266Initialized
-    }
-
-
-
-    /**
-     * Initialize the ESP8266.
-     * @param tx Tx pin of micro:bit. eg: SerialPin.P16
-     * @param rx Rx pin of micro:bit. eg: SerialPin.P15
-     * @param baudrate UART baudrate. eg: BaudRate.BaudRate115200
-     */
-    //% weight=29
-    //% blockGap=40
-    //% blockId=esp8266_init
-    //% block="initialize ESP8266: Tx %tx Rx %rx Baudrate %baudrate"
-    export function init(tx: SerialPin, rx: SerialPin, baudrate: BaudRate) {
-        // Redirect the serial port.
-        serial.redirect(tx, rx, baudrate)
-        serial.setTxBufferSize(128)
-        serial.setRxBufferSize(128)
-
-        // Reset the flag.
-        esp8266Initialized = false
-
-        // Restore the ESP8266 factory settings.
-        if (sendCommand("AT+RESTORE", "ready", 5000) == false) return
-
-        // Turn off echo.
-        if (sendCommand("ATE0", "OK") == false) return
-
-        // Initialized successfully.
-        // Set the flag.
-        esp8266Initialized = true
-    }
-
-
-
-    /**
-     * Return true if the ESP8266 is connected to WiFi router.
-     */
-    //% weight=28
-    //% blockGap=8
-    //% blockId=esp8266_is_wifi_connected
-    //% block="WiFi connected"
-    export function isWifiConnected(): boolean {
-        // Get the connection status.
-        sendCommand("AT+CIPSTATUS")
-        let status = getResponse("STATUS:", 1000)
-
-        // Wait until OK is received.
-        getResponse("OK")
-
-        // Return the WiFi status.
-        if ((status == "") || status.includes("STATUS:5")) {
-            return false
-        }
-        else {
-            return true
-        }
-    }
-
-
-
-    /**
-     * Connect to WiFi router.
-     * @param ssid Your WiFi SSID.
-     * @param password Your WiFi password.
-     */
-    //% weight=27
-    //% blockGap=8
-    //% blockId=esp8266_connect_wifi
-    //% block="connect to WiFi: SSID %ssid Password %password"
-    export function connectWiFi(ssid: string, password: string) {
-        // Set to station mode.
-        sendCommand("AT+CWMODE=1", "OK")
-
-        // Connect to WiFi router.
-        sendCommand("AT+CWJAP=\"" + ssid + "\",\"" + password + "\"", "OK", 20000)
-    }
-
-
-
-    
 }
