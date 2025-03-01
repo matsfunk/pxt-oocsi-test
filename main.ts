c97ecc774d3752f8223ecd09ff83da31294240c1
matsfunk <mathias.funk@gmail.com>
2025-03-01 14:51:31 +0100

    block comments minus dbug

--- main.ts
+++ main.ts
@@ -15,8 +15,6 @@ namespace oocsi {
     let lastMessage : { [key: string] : { value: any } } = {};
     let newData = false;
 
-    export let inputProcessed = 0;
-
     /**
      * Connect to OOCSI
      * @param server OOCSI server. eg: "super.oocsi.net"
@@ -25,7 +23,7 @@ namespace oocsi {
     //% weight=20
     //% blockGap=8
     //% blockId=oocsi_connect
-    //% block="connect to OOCSI: Server %channel Name %name"
+    //% block="connect to OOCSI server %server with name %name"
     export function connect(server: string, name: string) {
 
         sendCommand("AT+CIPSTART=\"TCP\",\"" + server + "\",4444", "OK", 10000)
@@ -38,28 +36,18 @@ namespace oocsi {
 
         serial.onDataReceived("\n", () => {
 
-            led.plotBrightness(0, 0, 0)
-            led.plotBrightness(1, 0, 0)
-            led.plotBrightness(2, 0, 0)
-            led.plotBrightness(3, 0, 0)
-            
-            pause(10);
+            // small delay before send
+            pause(10)
 
             let line: string = getResponse("+IPD", 500)
-
-            led.plotBrightness(0, 0, 255)
 
             // nothing received?
             if(line == undefined || line.trim().length == 0) {
                 return;
             }
 
-            led.plotBrightness(1, 0, 255)
-
             // respond to ping
             if(line.includes('ping') && !line.includes('{')) {
-
-                inputProcessed++
 
                 // // Make sure the WiFi is connected.
                 // if (isWifiConnected() == false) return
@@ -72,8 +60,6 @@ namespace oocsi {
                 return
             }
 
-            led.plotBrightness(2, 0, 255)
-
             // try parse line
             try {
                 const jsonString = line.substr(line.indexOf('{'));
@@ -82,18 +68,14 @@ namespace oocsi {
                     lastMessage = temp;
                     newData = true
                 }
-
-                led.plotBrightness(3, 0, 255)
-
             } catch(err) {
                 lastMessage = {}            
             }
-
         });
     }
 
     /**
-     * Send to OOCSI
+     * Send data to OOCSI channel
      * @param channel OOCSI channel.
      * @param key Key to send.
      * @param value Value to send.
@@ -101,11 +83,14 @@ namespace oocsi {
     //% weight=19
     //% blockGap=8
     //% blockId=oocsi_send
-    //% block="send to OOCSI: Channel %channel Key %key Value %value"
+    //% block="send Key %key Value %value to OOCSI channel %channel"
     export function send(channel: string, key: string, value: string) {
 
         // Make sure the WiFi is connected.
         if (isWifiConnected() == false) return
+
+        // small delay before send
+        pause(10)
 
         // prepare and send data
         let data = `sendjson ${channel} {"${key}": "${value}"}` + "\r\n"
@@ -116,68 +101,25 @@ namespace oocsi {
 
     /**
      * Subscribe to OOCSI channel
-     * @param channel OOCSI channel.
+     * @param channel OOCSI channel. eg: "testchannel"
      */
     //% weight=18
     //% blockGap=8
     //% blockId=oocsi_subscribe
-    //% block="subscribe to OOCSI: Channel %channel
+    //% block="subscribe to OOCSI channel %channel
     export function subscribe(channel: string) {
 
         // Make sure the WiFi is connected.
         if (isWifiConnected() == false) return
 
+        // small delay before send
+        pause(10)
+
         // prepare and send data
         let data = `subscribe ${channel}` + "\r\n"
         sendCommand("AT+CIPSEND=" + (data.length + 2))
         sendCommand(data)
-
     }
-
-    // /**
-    //  * Check for new incoming OOCSI messages
-    //  */
-    // //% weight=17
-    // //% blockGap=8
-    // //% blockId=oocsi_check
-    // //% block="check OOCSI"
-    // export function check() : boolean {
-
-    //     // Make sure the WiFi is connected.
-    //     if (isWifiConnected() == false) return false
-
-    //     let line: string = getResponse("+IPD", 500)
-
-    //     // nothing received?
-    //     if(line == undefined || line.trim().length == 0) {
-    //         return false;
-    //     }
-
-    //     // respond to ping
-    //     if(line.includes('ping') && !line.includes('{')) {
-
-    //         // // Make sure the WiFi is connected.
-    //         // if (isWifiConnected() == false) return false
-
-    //         // // send response
-    //         // let data = `.\r\n`
-    //         // sendCommand("AT+CIPSEND=" + (data.length + 2))
-    //         // sendCommand(data)
-
-    //         return false
-    //     }
-
-    //     // try parse line
-    //     try {
-    //         const jsonString = line.substr(line.indexOf('{'));
-    //         lastMessage = JSON.parse(jsonString)
-    //         return true
-    //     } catch(err) {
-    //         lastMessage = {}
-    //         return false            
-    //     }
-
-    // }
 
 
     /**
@@ -199,8 +141,8 @@ namespace oocsi {
 
     /**
      * Get a value from the last OOCSI message
-     * @param key Key of the value.
-     * @param defaultValue Default value in case there is no value for the key.
+     * @param key Key of the value. eg: "color"
+     * @param defaultValue Default value in case there is no value for the key. eg: "-1"
      */
     //% weight=16
     //% blockGap=8
