ef6fa57320b1719ac1bd2bd13739ab54c3ba395c
matsfunk <mathias.funk@gmail.com>
2025-03-02 21:06:47 +0100

    test ts

---
+++ test.ts
@@ -0,0 +1,30 @@
+const TX_PIN = SerialPin.P0
+const RX_PIN = SerialPin.P1
+const BAUDRATE = BaudRate.BaudRate115200
+
+const WIFI_SSID = "my_ssid"
+const WIFI_PWD = "my_password"
+
+const OOCSI_SERVER = "super.oocsi.net"
+const OOCSI_NAME = "microbit_test_###"
+
+
+// Initialize the ESP8266 module.
+// Show sad face if failed.
+oocsi.init(TX_PIN, RX_PIN, BAUDRATE)
+if (!oocsi.isESP8266Initialized()) {
+    basic.showIcon(IconNames.Sad)
+    basic.pause(1000)
+}
+
+// Connect to WiFi router.
+// Show sad face if failed.
+oocsi.connectWiFi(WIFI_SSID, WIFI_PWD)
+if (!oocsi.isWifiConnected()) {
+    basic.showIcon(IconNames.Sad)
+    basic.pause(1000)
+}
+
+// Connect to WiFi router.
+// Show sad face if failed.
+oocsi.connect(OOCSI_SERVER, OOCSI_NAME)
